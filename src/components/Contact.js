31ffec2ea4f6d79396604cb55ec5c93bca968270
Nicolas Vera <57852440+queondatodotranqui@users.noreply.github.com>
2021-05-24 19:43:02 -0300

    fixed images to production

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,18 +12,18 @@ const Contact = ()=>{
                     <Nav>
                         <Col>
                             <Nav.Link href='https://www.linkedin.com/in/nicolas-vera-653b54196'>
-                                <Image src='/img/linkedin.svg'></Image>
+                                <Image src={process.env.PUBLIC_URL + '/img/linkedin.svg'}></Image>
                             </Nav.Link>
                             
                         </Col>
                         <Col>
                             <Nav.Link href='https://www.instagram.com/_________________________vera/'>
-                                <Image src='/img/instagram.svg'></Image>
+                                <Image src={process.env.PUBLIC_URL + '/img/instagram.svg'}></Image>
                             </Nav.Link>   
                         </Col>
                         <Col>
                             <Nav.Link href='https://github.com/queondatodotranqui'>
-                                <Image src='/img/github.svg'></Image>
+                                <Image src={process.env.PUBLIC_URL + '/img/github.svg'}></Image>
                             </Nav.Link>    
                         </Col>
                     </Nav>
